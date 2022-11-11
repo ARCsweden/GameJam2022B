6af910e119d06411e70cb0399ca4a6b013b8c08d
Erlatronics <arcsweden.se@gmail.com>
2022-11-11 21:42:34 +0100

    Added some frog movement

---
+++ Assets/Scripts/FrogAim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogAim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+     Vector3 lookAt = mouseScreenPosition;
+
+     float AngleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
+
+     float AngleDeg = (180 / Mathf.PI) * AngleRad;
+
+     this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+
+    
+}
